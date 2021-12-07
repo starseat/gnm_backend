9d7bd38926f264e0e34d59baed3a59a3c1de05a5
starseat <starseat3@gmail.com>
2021-12-07 18:17:45 +0900

    소스 수정

--- 09.src/gnm-server/app.js
+++ 09.src/gnm-server/app.js
@@ -2,8 +2,15 @@ const express = require("express");
 // express async/await 의 exception 처리
 const asyncify = require('express-asyncify');
 
-const morgan = require('morgan');
+const createError = require('http-errors');
+const path = require('path')
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');  // 변수명 morgan -> logger 로 변경
+
+
 const cors = require('cors')();
+
+const routes = require('./routes');
 
 const app = express();
 // const app = asyncify(express());  // asyncify 적용
@@ -13,8 +20,18 @@ const PORT = ((env === 'production') ? process.env.PROD_PORT : process.env.DEV_P
 
 require('dotenv').config();
 
+app.use(logger('dev'));
+/*
+logger(format)
+- format
+  + 'combined', 'tiny', 'dev', 'common'
+*/
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
+
+app.use(routes);
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -22,6 +39,10 @@ app.use(express.urlencoded({ extended: true}));
 //     err.status = 404;
 //     next(err);
 // });
+app.use(function(req, res, next) {
+    console.log('error 404');
+    next(createError(404));
+});
 
 // // error handler
 // app.use(function(err, req, res, next) {
@@ -35,9 +56,14 @@ app.use(express.urlencoded({ extended: true}));
 //     res.render('error');
 //     // res.json({ error: err.message || 'internal server error' });
 // });
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.get('/', (req, res) => {
-    res.send('gnm api server');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(PORT, () => {
